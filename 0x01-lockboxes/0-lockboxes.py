d007f135b4fbf338723285b7c3c2afdac246ca46
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-06-10 00:47:18 +0100

    submit task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    """Function to return true or false if all boxes can
+    be unlocked
+
+    Args:
+        boxes (box): a list of list to check
+
+    Returns:
+        _type_: True if all box can be unlocked or False if not
+    """
+    # Number of boxes
+    n = len(boxes)
+
+    # A set to keep track of which boxes have been opened
+    opened_boxes = set()
+
+    # A queue to manage keys we have to use to open other boxes
+    keys = [0]
+
+    while keys:
+        # Get a key (box number) from the queue
+        current_key = keys.pop()
+
+        # If we haven't already opened this box
+        if current_key not in opened_boxes:
+            # Mark this box as opened
+            opened_boxes.add(current_key)
+
+            # Add all the keys from this box to the queue
+            for key in boxes[current_key]:
+                if key < n and key not in opened_boxes:
+                    keys.append(key)
+
+    # If we've opened all the boxes, return True
+    return len(opened_boxes) == n
