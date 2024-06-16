fb3bdf1c3cbcc283cd6ede83800db6b353d4928f
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-06-16 03:54:56 +0100

    min operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""MINIMUM OPERATIONS"""
+
+def minOperations(n):
+    """Calculates the minimum operations"""
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
